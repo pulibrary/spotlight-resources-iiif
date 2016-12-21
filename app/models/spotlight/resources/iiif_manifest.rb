d572a95806d61c493ed4f428044e1a22e9a946e1
Trey Pendragon <tpendragon@princeton.edu>
2016-12-21 10:09:20 -0800

    Index custom fields as vocabs by default

--- app/models/spotlight/resources/iiif_manifest.rb
+++ app/models/spotlight/resources/iiif_manifest.rb
@@ -93,7 +93,7 @@ module Spotlight
 
       def create_sidecars_for(*keys)
         missing_keys(keys).each do |k|
-          exhibit.custom_fields.create! label: k, readonly_field: true
+          exhibit.custom_fields.create! label: k, readonly_field: true, field_type: "vocab"
         end
         @exhibit_custom_fields = nil
       end
